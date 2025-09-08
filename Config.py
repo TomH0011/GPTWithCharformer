648a902989e4008929ff337426d0142f72943a69
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-09-08 15:47:49 +0100

    optimised, refactored, still gibberish though

--- Config.py
+++ Config.py
@@ -1,4 +1,5 @@
 import torch
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print(f'using device: {device}')
@@ -7,15 +8,18 @@ print(f'using device: {device}')
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
     print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+    print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
 
-
-batch_size = 64
+batch_size = 256
 block_size = 63
-learning_rate = 0.00001
-epochs = 50
-max_iters = 5000
+learning_rate = 0.0001
+max_iters = 10000
 eval_interval = 500
-MODEL_DIM = 512
-DOWNSAMPLE_FACTOR = 3
+MODEL_DIM = 256
+DOWNSAMPLE_FACTOR = 3  # block size % DOWNSAMPLE_FACTOR must equal 0
+depth = 6  # Number of attention layers
+heads = 8  # Number of attention heads looking for patterns
+gbst_blocks = ((3, 0), (3, 1), (3, 2))
+weight_decay = 0.1
+dropout = 0.3
